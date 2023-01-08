5483470c80f54a6c42a066c9304e95f23a55acea
Mateusz Martin <mateusz.martinn@gmail.com>
2023-01-08 23:10:46 +0100

    Comments added\ removed additional console.logs\ changed some variables names

--- src/index.js
+++ src/index.js
@@ -15,53 +15,57 @@ const loadMoreBtn = document.querySelector('.footer__load-more');
 
 // Global Variables
 let currentPage = 1;
-let pagesLeft = 0;
+let imagesLeft = 0;
+
 ////////////////////////////
 
 async function fetchMain() {
+  // Get input's value
   const searchedKey = searchBar.value;
+  // Clear website
   imagesDisplay.innerHTML = ' ';
-  ////////////////////////////
+  // Search for images by keyword. Show 40 images per page and show specified page ( logic done bellow)
   await axios
     .get(
       `${API_URL}${API_KEY}&q=${searchedKey}&per_page=40&page=${currentPage}`
     )
     .then(response => {
+      // Save array of images objecs as variable
       const hits = response.data.hits;
-      console.log(`Response: `);
-      console.log(response);
-      /////////////////////////////////
+
+      // If promise is fuffiled
       if (response.statusText == 'OK') {
-        /////////////////////////////////
+        // If promise is fuffiled but notthing has been found
         if (response.data.totalHits == 0) {
           Notiflix.Notify.failure(
             'Sorry, there are no images matching your search query. Please try again.'
           );
           return;
         }
-        /////////////////////////////////
+        // Otherwise, show succes notify (only if user is on first page)
         if (currentPage == 1) {
           Notiflix.Notify.success(
             `Horray! We found ${response.data.totalHits} images!`
           );
         }
-        /////////////////////////////////
-        console.log(`Hits found: `);
-        console.log(hits);
+        // Call Rendering function for every image object found
         for (let hit of hits) {
           displayResults(hit);
         }
-        /////////////////////////////////
+        // Create SimpleLightbox Gallery
         const lightbox = new SimpleLightbox('.main-content>.image-result img');
-        /////////////////////////////////
-        pagesLeft = response.data.totalHits - 40 * currentPage;
-        console.log(pagesLeft);
-        if (pagesLeft <= 0) {
+
+        // Create Pagination Logic
+        imagesLeft = response.data.totalHits - 40 * currentPage;
+        console.log(`Images left: ${imagesLeft}`);
+        // If There is no images left - hide "Load More..." button and show notify
+        if (imagesLeft <= 0) {
           loadMoreBtn.style.display = 'none';
           Notiflix.Notify.info(
             "We're sorry, but you've reached the end of search results."
           );
         } else {
+          // Otherwise show "Load More..." button
           loadMoreBtn.style.display = 'block';
         }
       }
@@ -69,6 +73,7 @@ async function fetchMain() {
 }
 
 function displayResults(image) {
+  // HTML structure of result
   const resultNode = `<img
       src="${image.previewURL}"
       alt="${image.tags}"
@@ -93,6 +98,7 @@ function displayResults(image) {
         <p class="image-result__stats--data">${image.downloads}</p>
       </li>
     </ul>`;
+    // Adding results to DOM
   let resultElement = document.createElement('article');
   resultElement.innerHTML = resultNode;
   resultElement.classList.add('image-result');
@@ -100,15 +106,19 @@ function displayResults(image) {
 }
 /////////////////////////////////
 function loadMore() {
+  // Increment current page counter AND THEN call fetch function
   currentPage++;
   fetchMain();
 }
 function newSearch() {
+  // Reset current page counter AND THEN call fetch function
   currentPage = 1;
   fetchMain();
 }
-/////////////////////////////////
+
+// Event Listeners (done indirectly because of need to modify some values before callbacks)
 searchIcon.addEventListener('click', newSearch);
 loadMoreBtn.addEventListener('click', loadMore);
 
+// Hide "Load More..." button on default
 loadMoreBtn.style.display = 'none';
